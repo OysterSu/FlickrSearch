ba0d01292119eb4f080574992ebad866a88f65f2
蘇健豪 <jenhausu@icloud.com>
2020-01-14 17:09:23 +0800

    Initial Commit

---
+++ FlickerSearch/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  FlickerSearch
+//
+//  Created by 蘇健豪1 on 2020/1/14.
+//  Copyright © 2020 Oyster. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
