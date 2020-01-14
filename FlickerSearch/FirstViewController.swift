3185dd3006ea0555d877e96166e85244a19e3da7
蘇健豪 <jenhausu@icloud.com>
2020-01-14 17:43:46 +0800

    Basic Setup

--- FlickerSearch/FirstViewController.swift
+++ FlickerSearch/FirstViewController.swift
@@ -8,13 +8,57 @@
 
 import UIKit
 
-class FirstViewController: UIViewController {
+class FirstViewController: UIViewController, UITextFieldDelegate {
 
+    @IBOutlet weak var searchTextField: UITextField!
+    @IBOutlet weak var numberTextField: UITextField!
+    @IBOutlet weak var searchButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        viewSetup()
+    }
+    
+    private func viewSetup() {
+        searchTextField.delegate = self
+        numberTextField.delegate = self
+        numberTextField.keyboardType = .numberPad
+        
+        searchButton.isEnabled = false
+        searchButton.setTitleColor(.red, for: .disabled)
     }
 
-
+    @IBAction func Search(_ sender: UIButton) {
+        
+    }
+    
+    // MARK: - TextField
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        guard searchTextField.text != "" else {
+            searchButton.isEnabled = false
+            return
+        }
+        
+        guard numberTextField.text != "" else {
+            searchButton.isEnabled = false
+            return
+        }
+        
+        searchButton.isEnabled = true
+    }
+    
+    // MARK: Fold Keyboard
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+          textField.resignFirstResponder()
+          return true
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+    
 }
 
